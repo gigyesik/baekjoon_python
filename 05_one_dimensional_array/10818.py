149528ecb3041123bdf0061e6e3a1da5ee1a5689
WoongBeom Heo <98306498+gigyesik@users.noreply.github.com>
2022-01-27 21:16:45 +0900

    def min, max (not use standard library)

--- 05_one_dimensional_array/10818.py
+++ 05_one_dimensional_array/10818.py
@@ -2,13 +2,37 @@
 
 import sys
 
+case = int(input())
+num = [None] * case
+a = sys.stdin.readline().split()
+
+def get_min() -> int:
+    for i in range(case):
+        num[i] = int(a[i])
+    temp = num[0]
+    for j in range(1, case):
+        if num[j] <= num[j - 1]:
+            temp = num[j]
+        else:
+            pass
+    return temp
+
+def get_max() -> int:
+    for i in range(case):
+        num[i] = int(a[i])
+    temp = num[0]
+    for j in range(1, case):
+        if num[j] >= num[j - 1]:
+            temp = num[j]
+        else:
+            pass
+    return temp
+
 def minmax() -> int:
-    case = int(input())
-    num = [None] * case
-    a = sys.stdin.readline().split()
+
     for i in (range(case)):
         num[i] = int(a[i])
-    print(min(num), max(num), end=' ')
+    print(get_min(), get_max(), end=' ')
 
 
 minmax()
