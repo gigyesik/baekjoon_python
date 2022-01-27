ef925895a3b44834c5bfa054258b08e2e823a37f
WoongBeom Heo <98306498+gigyesik@users.noreply.github.com>
2022-01-28 00:22:50 +0900

    correct wrong logic

--- 05_one_dimensional_array/10818.py
+++ 05_one_dimensional_array/10818.py
@@ -3,28 +3,28 @@
 
 import sys
 
-def get_min(case: int, num: list) -> int:
+def get_min(length: int, num: list) -> int:
     temp = num[0]
-    for j in range(1, case):
-        if num[j] <= num[j - 1]:
+    for j in range(1, length):
+        if num[j] <= temp:
             temp = num[j]
         else:
             pass
     return temp
 
-def get_max(case: int, num: list) -> int:
+def get_max(length: int, num: list) -> int:
     temp = num[0]
-    for j in range(1, case):
-        if num[j] >= num[j - 1]:
+    for j in range(1, length):
+        if num[j] >= temp:
             temp = num[j]
         else:
             pass
     return temp
 
 def main():
-    case = int(input())
-    a = sys.stdin.readline().split()
-    num = list(map(int, a))
-    print(get_min(case, num), get_max(case, num))
+    length = int(input())
+    numin = sys.stdin.readline().split()
+    num = list(map(int, numin))
+    print(get_min(length, num), get_max(length, num))
     
 main()
