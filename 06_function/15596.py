0e4a19df70ed7683ccc7160b931fb5e47e702d7c
WoongBeom Heo <98306498+gigyesik@users.noreply.github.com>
2022-01-28 17:40:21 +0900

    sum of integer (only function definition)

---
+++ 06_function/15596.py
@@ -0,0 +1,19 @@
+# 정수 N개의 합
+# https://www.acmicpc.net/problem/15596
+
+import sys
+
+def solve(a: list) -> int:
+    ans = 0
+    for i in range(len(a)):
+        ans += int(a[i])
+    return ans
+        
+"""
+def main():
+    a = sys.stdin.readline().split()
+    list_a = list(map(int, a))
+    print(sum(list_a))
+
+main()
+"""
