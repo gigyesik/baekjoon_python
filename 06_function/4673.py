0bb574ca306e52ed5486c058a06132c5cb828488
WoongBeom Heo <98306498+gigyesik@users.noreply.github.com>
2022-01-30 00:34:30 +0900

    reduce runtime

--- 06_function/4673.py
+++ 06_function/4673.py
@@ -1,27 +1,26 @@
 # 셀프 넘버
 # https://www.acmicpc.net/problem/4673
 
-def dn(n: int) -> int:
+def d(n: int) -> int:
     strn = str(n)
     for i in strn:
         n += int(i)
     return n
 
-def selfnum(n: int) -> int:
-    result = n
-    for i in range(n+1):
-        if dn(i) == n:
-            return
+def selfnum(n: int) -> list:
+    numlist = list(range(1, n+1))
+    
+    for i in range(1, n+1):
+        if d(i) not in numlist:
+            pass
         else:
-            result = n
-    return result
+            numlist.remove(d(i))
+
+    return numlist
 
 
 def main(n):
-    for i in range(1, n+1):
-        if selfnum(i) == None:
-            pass
-        else:
-            print(selfnum(i))
+    for i in range(len(selfnum(n))):
+        print(selfnum(n)[i])
 
 main(10000)
