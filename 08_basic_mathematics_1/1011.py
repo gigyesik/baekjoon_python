aa036afb957a5a427befde3b5b7015955800fa50
WoongBeom Heo <98306498+gigyesik@users.noreply.github.com>
2022-02-06 14:00:51 +0900

    Create 1011.py

---
+++ 08_basic_mathematics_1/1011.py
@@ -0,0 +1,39 @@
+# Fly me to the Alpha Centauri
+# https://www.acmicpc.net/problem/1011
+
+import sys
+
+def fmttac(xy: list) -> int:
+    x = xy[0]
+    y = xy[1]
+    length = y - x
+    sumn = 1
+    n = 1
+    remain = length - sumn
+
+    while sumn < length:
+        sumn = n * (n + 1)
+        remain = length - sumn
+        if sumn > length:
+            n -= 1
+            sumn = n * (n + 1)
+            remain = length - sumn
+            break
+        else:
+            n += 1
+
+    if remain > 2 * n:
+        return 2 * n + 2
+    else:
+        return 2 * n + 1
+
+def main():
+    case = int(input())
+    for _ in range(case):
+        xy = sys.stdin.readline().split()
+        listxy = list(map(int, xy))
+        print(fmttac(listxy))
+
+main()
+
+# fail
